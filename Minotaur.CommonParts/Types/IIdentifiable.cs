553b018bc39dc1ca2ba9270ca7adcb2aaeb69713
Yaroslav Vasylenko <ryandarkyv@gmail.com>
2019-06-14 16:43:06 +0300

    Init JsonWebToken

---
+++ Minotaur.CommonParts/Types/IIdentifiable.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Minotaur.CommonParts.Types
+{
+    public interface IIdentifiable
+    {
+        Guid Id { get; }
+    }
+}
---
+++ Minotaur.CommonParts/Types/MinotaurException.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Minotaur.CommonParts.Types
+{
+    public class MinotaurException : Exception
+    {
+        public string Code { get; }
+
+        public MinotaurException()
+        {
+        }
+
+        public MinotaurException(string code)
+        {
+            Code = code;
+        }
+
+        public MinotaurException(string message, params object[] args) 
+            : this(string.Empty, message, args)
+        {
+        }
+
+        public MinotaurException(string code, string message, params object[] args) 
+            : this(null, code, message, args)
+        {
+        }
+
+        public MinotaurException(Exception innerException, string message, params object[] args)
+            : this(innerException, string.Empty, message, args)
+        {
+        }
+
+        public MinotaurException(Exception innerException, string code, string message, params object[] args)
+            : base(string.Format(message, args), innerException)
+        {
+            Code = code;
+        }        
+    }
+}
