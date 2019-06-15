f33b92950d80bfeb350cbbf90f0d1a3cecbdb2bb
Yaroslav Vasylenko <ryandarkyv@gmail.com>
2019-06-15 21:26:52 +0300

    Added command, event and query handlers

---
+++ Minotaur.CommonParts/Handlers/ICommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Minotaur.CommonParts.Messages;
+using Minotaur.CommonParts.RabbitMq;
+
+namespace Minotaur.CommonParts.Handlers
+{
+    public interface ICommandHandler<in TCommand> where TCommand : ICommand
+    {
+        Task HandleAsync(TCommand command, ICorrelationContext context);
+    }
+}
---
+++ Minotaur.CommonParts/Handlers/IHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Minotaur.CommonParts.Types;
+
+namespace Minotaur.CommonParts.Handlers
+{
+    public interface IHandler
+    {
+        IHandler Handle(Func<Task> handle);
+        IHandler OnSuccess(Func<Task> onSuccess);
+        IHandler OnError(Func<Exception, Task> onError, bool rethrow = false);
+        IHandler OnCustomError(Func<MinotaurException, Task> onCustomError, bool rethrow = false);
+        IHandler Always(Func<Task> always);
+        Task ExecuteAsync();
+    }
+}
