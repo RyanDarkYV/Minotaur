3f3f277849559b9544a27a4965c9071156008b1e
Yaroslav Vasylenko <ryandarkyv@gmail.com>
2019-06-15 21:47:42 +0300

    Added ServiceId and JsonParser and ErrorHanderMiddleware

---
+++ Minotaur.CommonParts/Mvc/ServiceId.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Minotaur.CommonParts.Mvc
+{
+    public class ServiceId
+    {
+        private static readonly string UniqueId = $"{Guid.NewGuid():N}";
+
+        public string Id => UniqueId;
+    }
+}
