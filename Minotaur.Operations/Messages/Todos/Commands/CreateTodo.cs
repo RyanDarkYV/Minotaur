949e143b3df93725e2e9f1d04cf611e7c37cf18d
Yaroslav Vasylenko <ryandarkyv@gmail.com>
2019-06-19 17:27:56 +0300

    Added commands and events for todos operations

---
+++ Minotaur.Operations/Messages/Todos/Commands/CreateTodo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Minotaur.CommonParts.Messages;
+using Newtonsoft.Json;
+
+namespace Minotaur.Operations.Messages.Todos.Commands
+{
+    [MessageNamespace("todos")]
+    public class CreateTodo : ICommand
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+        public Guid UserId { get; set; }
+
+        [JsonConstructor]
+        public CreateTodo(Guid id, string title, string description, bool isDone, Guid userId)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            IsDone = isDone;
+            UserId = userId;
+        }
+    }
+}
---
+++ Minotaur.Operations/Messages/Todos/Events/TodoItemCreated.cs
@@ -0,0 +1,26 @@
+﻿using Minotaur.CommonParts.Messages;
+using Newtonsoft.Json;
+using System;
+
+namespace Minotaur.Operations.Messages.Todos.Events
+{
+    [MessageNamespace("todos")]
+    public class TodoItemCreated : IEvent
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+        public Guid UserId { get; set; }
+
+        [JsonConstructor]
+        public TodoItemCreated(Guid id, string title, string description, bool isDone, Guid userId)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            IsDone = isDone;
+            UserId = userId;
+        }
+    }
+}
