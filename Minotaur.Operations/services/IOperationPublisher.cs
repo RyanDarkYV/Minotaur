b8c1418f59730010294b0078024ea36ace719c0a
Yaroslav Vasylenko <ryandarkyv@gmail.com>
2019-06-19 13:48:20 +0300

    Added services for OperationsService

---
+++ Minotaur.Operations/services/IOperationPublisher.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Minotaur.CommonParts.RabbitMq;
+
+namespace Minotaur.Operations.Services
+{
+    public interface IOperationPublisher
+    {
+        Task PendingAsync(ICorrelationContext context);
+        Task CompleteAsync(ICorrelationContext context);
+        Task RejectedAsync(ICorrelationContext context, string code, string message);
+    }
+}
---
+++ Minotaur.Operations/services/IOperationsStorage.cs
@@ -0,0 +1,14 @@
+﻿using Minotaur.Operations.Dto;
+using System;
+using System.Threading.Tasks;
+
+namespace Minotaur.Operations.Services
+{
+    public interface IOperationsStorage
+    {
+        Task<OperationDto> GetAsync(Guid id);
+
+        Task SetAsync(Guid id, Guid userId, string name, OperationState state, string resource, string code = null,
+            string reason = null);
+    }
+}
