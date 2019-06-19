97d19f75ca212789928076203dbd021ab0960efa
Yaroslav Vasylenko <ryandarkyv@gmail.com>
2019-06-19 17:44:10 +0300

    Added API controllers for operations service

---
+++ Minotaur.Operations/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Minotaur.Operations.Controllers
+{
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("Minotaur Operations Service");
+    }
+}
