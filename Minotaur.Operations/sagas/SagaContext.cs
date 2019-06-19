17834ee7a54093566700b162f83221d5ca7dde75
Yaroslav Vasylenko <ryandarkyv@gmail.com>
2019-06-19 17:13:54 +0300

    Added sagacontext and operation dto

---
+++ Minotaur.Operations/sagas/SagaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Chronicle;
+using Minotaur.CommonParts.RabbitMq;
+
+namespace Minotaur.Operations.Sagas
+{
+    public class SagaContext : ISagaContext
+    {
+        public Guid CorrelationId { get; }
+        public string Originator { get; }
+
+        public SagaContext(Guid correlationId, string originator) =>
+            (CorrelationId, Originator) = (correlationId, originator);
+
+        public static ISagaContext Empty => new SagaContext(Guid.Empty, string.Empty);
+        public static ISagaContext FromCorrelationContext(ICorrelationContext context) => new SagaContext(context.Id, context.Resource);
+    }
+}
