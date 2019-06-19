c49aca29cde4aa659e750d49ae174d1b1301ae03
Yaroslav Vasylenko <ryandarkyv@gmail.com>
2019-06-19 17:43:30 +0300

    Added events for Identity service

---
+++ Minotaur.Identity/Messages/Events/AccessTokenRefreshed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Minotaur.CommonParts.Messages;
+using Newtonsoft.Json;
+
+namespace Minotaur.Identity.Messages.Events
+{
+    public class AccessTokenRefreshed : IEvent
+    {
+        public Guid UserId { get; }
+
+        [JsonConstructor]
+        public AccessTokenRefreshed(Guid userId)
+        {
+            UserId = userId;
+        }
+    }
+}
---
+++ Minotaur.Identity/Messages/Events/SignInRejected.cs
@@ -0,0 +1,20 @@
+﻿using Minotaur.CommonParts.Messages;
+using Newtonsoft.Json;
+
+namespace Minotaur.Identity.Messages.Events
+{
+    public class SignInRejected : IRejectedEvent
+    {
+        public string Email { get; }
+        public string Reason { get; }
+        public string Code { get; }
+
+        [JsonConstructor]
+        public SignInRejected(string email, string reason, string code)
+        {
+            Email = email;
+            Reason = reason;
+            Code = code;
+        }
+    }
+}
